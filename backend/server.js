972381de38ae38ce34018101320028845a78cb75
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-04-07 17:10:24 +0530

    Login SignUp backend part completed

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ const dotenv = require("dotenv")
 const chats = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRouts = require('./routes/userRoutes');
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 
 dotenv.config();
 connectDB();
 const app = express();
+
+app.use(express.json());
 
 
 app.get("/", (req, res) => {
@@ -15,14 +19,10 @@ app.get("/", (req, res) => {
 })
 
 
-app.get("/api/chat", (req, res) => {
-    res.send(chats)
-})
+app.use('/api/user', userRouts)
 
-app.get("/api/chat/:id", (req, res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 8000
 
