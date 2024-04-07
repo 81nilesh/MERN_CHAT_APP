0dc76ea8636cc97c65fa04e3bc54cfe7886f6135
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-04-07 11:03:25 +0530

    mongoDb Connection successfully

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv")
 const chats = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
-const app = express();
+
 dotenv.config();
+connectDB();
+const app = express();
 
 
 app.get("/", (req, res) => {
@@ -22,4 +26,4 @@ app.get("/api/chat/:id", (req, res) => {
 
 const PORT = process.env.PORT || 8000
 
-app.listen(8000, console.log(`Server Started on PORT ${PORT}`));
+app.listen(8000, console.log(`Server Started on PORT ${PORT}`.yellow.bold));
