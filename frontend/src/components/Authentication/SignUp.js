906830ba72cd0cc80f88fe09d41fd1c45e0c0854
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-04-08 11:16:33 +0530

    Backend

--- frontend/src/components/Authentication/SignUp.js
+++ frontend/src/components/Authentication/SignUp.js
@@ -2,7 +2,7 @@ import { Button, FormControl, FormLabel, Input, InputGroup, InputRightElement, V
 import React, { useState } from 'react';
 import { useToast } from '@chakra-ui/react'
 import axios from "axios";
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
     const [show, setShow] = useState(false);
